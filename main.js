107632b22b683999198821c306391212e0c771d3
Trevor Guinn <tdguinn87@gmail.com>
2020-08-18 20:28:24 -0500

    Created Javascript

---
+++ main.js
@@ -0,0 +1,30 @@
+let enterButton = document.getElementById("enter");
+let input = document.getElementById("userInput");
+let ul = document.querySelector("ul");
+let item = document.getElementsByTagName("li");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function listLength() {
+    return item.length;
+}
+
+function createListElement() {
+    let li = document.createElement("li"); // creates an element "li"
+    li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+    ul.appendChild(li); //adds li to ul
+    input.value = ""; //reset text input field
+
+
+//Start Strikethrough
+// because it's in the function, it only adds it for new items
+function crossOut() {
+    li.classList.toggle("done");
+}
+
+li.addEventListener("click",crossOut);
+}
+
+}
